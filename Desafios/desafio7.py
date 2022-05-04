c5a7cb5acd55d4b83547ec2404ec1a3eac5931e3
Jane Thais Oliveira <87083965+JaneOliveira@users.noreply.github.com>
2022-05-04 20:02:46 -0300

    desafio7+seloListas

---
+++ Desafios/desafio7.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 19:46:43 2022
+
+@author: jane.oliveira_dp6
+"""
+## O programa abaixo deve calcular a média dos valores digitados pelo usuário.
+## No entanto, ele não está funcionando bem. Você pode consertá-lo?
+
+def calcular_media(valores):
+    soma = 0.0
+    for i, valor in enumerate(valores):
+        soma += valor
+        i += 1
+    return soma / i
+
+continuar = True
+valores = []
+
+while continuar:
+    valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
+    if valor.lower() == 'ok':
+        break
+    valores.append(int(valor))
+
+media = calcular_media(valores)
+print('A média calculada para os valores {} foi de {}'.format(valores, media))
