dbc6ff71304efe6399104adcf8c317181593f5c8
Jane Thais Oliveira <87083965+JaneOliveira@users.noreply.github.com>
2022-06-07 00:12:24 -0300

    Update desafio14.py

--- Desafios/desafio14.py
+++ Desafios/desafio14.py
@@ -5,30 +5,44 @@ Created on Fri May 13 18:57:49 2022
 @author: jane.oliveira
 """
 
-
-import sys
 import requests
 import json
 
 # Crie uma aplicação que lê na linha de comando o nome de um feitiço
-
-
 # Utilize a biblioteca requests para ler o JSON disponível em
 # https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json
-
-
 # Converta o CSV para um dicionário cujas chaves são as palavras do cabeçalho do CSV.
-
-
 # Imprima para o usuário os dados sobre o feitiço que ele solicitou.
 # Se o feitiço não foi encontrado, lance uma exceção.
 
-# requisicao = requests.get('https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json')
-# print(requisição.json())
+def create_dic():
+    try:
+        speels = requests.get('https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json')
+        speels_json = speels.text
+        speels_dic = json.loads(speels_json.lower())
+        return speels_dic
+    except:
+        print("Sorry, a base de dados de feitiço encontra-se indiponivél no momento, tente mais tarde!")
+    
+
+def search_spell():
+    input_speel = input("Digite um feitiço que queira saber as informações: ").lower()
+    teste = list()
+    speels_dic = create_dic()
+    for key, value in speels_dic.items():
+        teste.append(value)
+
+    aux = -1
+    for i in range(len(teste)):
+        if teste[i]["encantamento"] == input_speel:
+            aux = i
+            
+    if(aux != -1):
+        print("\n\n\t INFORMAÇÕES \n")
+        print(f"{teste[aux]}")
+    else:
+        print("\nSorry, feitiço não encontrado!!\n\n")
 
 
-try:
-    page1 = requests.get('https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json', verify=False)
-
-except requests.exceptions.ConnectionError:
-    requests.status_code = "Connection refused"
+search_spell()
+    
